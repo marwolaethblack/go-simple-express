4ea159b26e5821647d98c6f5d4f4a470e7315d40
marwolaethblack <a.unal677@gmail.com>
2018-04-01 22:51:09 +0200

    initial commit

---
+++ simpleExpress.go
@@ -0,0 +1,117 @@
+package express
+
+import (
+	"fmt"
+	"net/http"
+)
+
+//Routes is the type in which your types are stored
+//It is a map where the key is string and the value is another map whose key is a string and value is a slice of functions
+type Routes map[string]map[string][]func(w http.ResponseWriter, req *http.Request, stop func())
+
+//MiddlewareHandler is the type your middleware functions passed to the app methods should be
+type MiddlewareHandler func(w http.ResponseWriter, req *http.Request, stop func())
+
+type appmethods interface {
+	Get(path string, handlers ...func(w http.ResponseWriter, req *http.Request, stop func()))
+	Post(path string, handlers ...func(w http.ResponseWriter, req *http.Request, stop func()))
+	Put(path string, handlers ...func(w http.ResponseWriter, req *http.Request, stop func()))
+	Delete(path string, handlers ...func(w http.ResponseWriter, req *http.Request, stop func()))
+	Patch(path string, handlers ...func(w http.ResponseWriter, req *http.Request, stop func()))
+}
+
+/*App is the type of the application with the relevant methods
+You need to make a new variable of this type first then use the relevant methodss
+*/
+type App struct {
+	routes Routes
+}
+
+//Get handles a GET http request to the given path
+func (a *App) Get(path string, handlers ...func(w http.ResponseWriter, req *http.Request, stop func())) {
+	if a.routes == nil {
+		a.routes = make(map[string]map[string][]func(w http.ResponseWriter, req *http.Request, stop func()))
+	}
+	if a.routes[path] == nil {
+		a.routes[path] = make(map[string][]func(w http.ResponseWriter, req *http.Request, stop func()))
+
+	}
+	if a.routes[path]["GET"] == nil {
+		a.routes[path]["GET"] = make([]func(w http.ResponseWriter, req *http.Request, stop func()), 5)
+	}
+	a.routes[path]["GET"] = handlers
+}
+
+//Post handles a POST http request to the given path
+func (a *App) Post(path string, handlers ...func(w http.ResponseWriter, req *http.Request, stop func())) {
+	if a.routes == nil {
+		a.routes = make(map[string]map[string][]func(w http.ResponseWriter, req *http.Request, stop func()))
+	}
+	if a.routes[path] == nil {
+		a.routes[path] = make(map[string][]func(w http.ResponseWriter, req *http.Request, stop func()))
+
+	}
+	if a.routes[path]["POST"] == nil {
+		a.routes[path]["POST"] = make([]func(w http.ResponseWriter, req *http.Request, stop func()), 5)
+	}
+	a.routes[path]["POST"] = handlers
+}
+
+//Put handles a PUT http request to the given path
+func (a *App) Put(path string, handlers ...func(w http.ResponseWriter, req *http.Request, stop func())) {
+	if a.routes == nil {
+		a.routes = make(map[string]map[string][]func(w http.ResponseWriter, req *http.Request, stop func()))
+	}
+	if a.routes[path] == nil {
+		a.routes[path] = make(map[string][]func(w http.ResponseWriter, req *http.Request, stop func()))
+
+	}
+	if a.routes[path]["PUT"] == nil {
+		a.routes[path]["PUT"] = make([]func(w http.ResponseWriter, req *http.Request, stop func()), 5)
+	}
+	a.routes[path]["PUT"] = handlers
+}
+
+//Delete handles a DELETE http request to the given path
+func (a *App) Delete(path string, handlers ...func(w http.ResponseWriter, req *http.Request, stop func())) {
+	if a.routes == nil {
+		a.routes = make(map[string]map[string][]func(w http.ResponseWriter, req *http.Request, stop func()))
+	}
+	if a.routes[path] == nil {
+		a.routes[path] = make(map[string][]func(w http.ResponseWriter, req *http.Request, stop func()))
+
+	}
+	if a.routes[path]["DELETE"] == nil {
+		a.routes[path]["DELETE"] = make([]func(w http.ResponseWriter, req *http.Request, stop func()), 5)
+	}
+	a.routes[path]["DELETE"] = handlers
+}
+
+func (a App) ServeHTTP(w http.ResponseWriter, req *http.Request) {
+	path := req.URL.Path
+	method := req.Method
+	handlers := a.routes[path][method]
+
+	stopflag := false
+	stop := func() {
+		stopflag = true
+	}
+
+	if handlers != nil {
+		for _, fn := range handlers {
+			if stopflag {
+				break
+			}
+			fn(w, req, stop)
+		}
+	} else {
+		fmt.Fprintf(w, "Cannot %v %v", method, path)
+	}
+
+}
+
+//Run starts the http server with the given port e.g ":8080", : is required
+func (a App) Run(port string) {
+	fmt.Printf("Server is running on localhost%s\n", port)
+	http.ListenAndServe(port, a)
+}
